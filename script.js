9ab02e9918a682391b8e74b4b0a32d9b55882b9f
Thunderback49 <24647cj@hvhs.school.nz>
2026-06-16 00:25:13 +0000

    Jun 16, 2026, 12:25 AM

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 console.log("Hello world!")
- 
+
+function getFormInput(){
+
+const NAME_FIELD = document.getElementById("nameField");
+const AGE_FIELD = document.getElementById("ageField");
+const BIRTHY_FIELD = document.getElementById("birthyField");
+
+let userName = NAME_FIELD.value;
+let userAge = AGE_FIELD.value;
+let birthYear = BIRTHY_FIELD.value;
+
+
+OUTPUT.innerHTML = "<p>Your name is "+userName+"</p>";
+OUTPUT.innerHTML +=("<p>Hi "+userName+" as of " +year+ " you are " +userAge+ " years old. You have " +money+ " dollars.</p>");
+OUTPUT.innerHTML +=("<p>you were born in " + birthYear+"</p>");
+OUTPUT.innerHTML +=("<p>in ten years you will be "+ oldage + " yeras old</p>");
+OUTPUT.innerHTML +=("<p>You have " +money+ " dollars</p>");
+OUTPUT.innerHTML +=("<p>you spent half your money, now you have " + halfmoney + " dollars</p>" );
+OUTPUT.innerHTML +=("<p>Then you got 3 dollars, now you have " +(halfmoney+3)+" dollars</p>");
+
+}
